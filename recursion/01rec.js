a1e32f83aae5542020c496a5bd8d0c9de68cb00b
Afzal97592 <mail2afzalahmad@gmail.com>
2025-04-04 17:36:24 +0530

    recursion

--- recursion/01rec.js
+++ recursion/01rec.js
@@ -92,7 +92,8 @@ function sumOfNaturalNum(n) {
 // factorial of a num using recursion
 
 function factorial(n) {
+  if (n < 0) return;
   if (n === 0 || n === 1) return 1;
-  return n * factorial(n - 1);
+  return factorial(n - 1) * n;
 }
-// console.log(factorial(20));
+console.log(factorial(10));
